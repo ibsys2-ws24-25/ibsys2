08d87c139bc70c068aec5307c93fe66ea78d5416
malikbredehoeft2 <malikbredehoeft@gmail.com>
2024-11-02 14:15:30 +0100

    Updated: Link to Production Plan

--- src/components/pages/periods/linkTable.tsx
+++ src/components/pages/periods/linkTable.tsx
@@ -19,9 +19,9 @@ export default function LinkTable() {
                     <TableRow>
                         <TableHead>Periods</TableHead>
                         <TableHead>Production Plan</TableHead>
-                        <TableHead>ProductP1</TableHead>
-                        <TableHead>ProductP2</TableHead>
-                        <TableHead>ProductP3</TableHead>
+                        <TableHead>Product P1</TableHead>
+                        <TableHead>Product P2</TableHead>
+                        <TableHead>Product P3</TableHead>
                         <TableHead>Workplace</TableHead>
                         <TableHead>Purchase Planning</TableHead>
                         <TableHead>Export</TableHead>
@@ -31,14 +31,14 @@ export default function LinkTable() {
                     {periodData.map((period, index) => (
                         <TableRow key={index} className="border-t">
                             <TableCell>
-                                <Link href={period.link} className="text-blue-500 hover:underline">
+                                <Link href="" className="text-blue-500 hover:underline">
                                     {period.period}
                                 </Link>
                             </TableCell>
                             <TableCell>
                                 <div className="flex justify-center items-center">
                                     <Button className="bg-primary text-primary-foreground">
-                                        <Link href="">PLAN</Link>
+                                        <Link href={`/periods/${period.period}/production-planning/1`}>Plan Production</Link>
                                     </Button>
                                 </div>
                             </TableCell>

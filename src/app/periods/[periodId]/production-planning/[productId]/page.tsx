392052aa6577e3ba65343a47219ce0f91c27375f
Leon Gauweiler <leon.gauweiler@gmail.com>
2024-11-02 14:07:59 +0100

    added: Heading for Planing

--- src/app/periods/[periodId]/production-planning/[productId]/page.tsx
+++ src/app/periods/[periodId]/production-planning/[productId]/page.tsx
@@ -1,8 +1,11 @@
 import MaterialTable from '@/components/pages/material/materialTable';
 
-export default async function HomePage() {
+export default async function HomePage({ params }: { params: { periodId: number, productId: number } }) {
+    console.log(params);
+
     return (
-        <div className="flex h-screen items-center justify-center">
+        <div>
+            <h1 className='text-2xl font-bold mb-4 text-primary'>Production Plan for Product {params.productId} in Period {params.periodId}</h1>
             <MaterialTable />
         </div>
     );

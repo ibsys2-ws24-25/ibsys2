fcfc6e50dc318067be615308c0d3068026e424b6
Leon Gauweiler <leon.gauweiler@gmail.com>
2024-11-02 14:26:41 +0100

    added: create period ui

--- src/app/periods/page.tsx
+++ src/app/periods/page.tsx
@@ -1,7 +1,11 @@
+import CreatePeriod from "@/components/pages/periods/CreatePeriod";
 import LinkTable from "@/components/pages/periods/linkTable"
 
 export default async function HomePage() {
     return (
-        <LinkTable/>
+        <div>
+            <CreatePeriod />
+            <LinkTable/>
+        </div>
     );
 }

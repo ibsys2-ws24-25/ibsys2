62966536aae0392e3f7ce55489b0fdffd1d2650a
Leon Gauweiler <leon.gauweiler@gmail.com>
2024-11-04 14:03:38 +0100

    changed: primary color

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <div className="flex items-center justify-between w-full px-6 py-4 bg-gray-800 text-white shadow-md">
-            <Link className="text-xl font-semibold" href="/">
+        <div className="flex items-center justify-between w-full px-6 py-4 bg-primary text-primary-foreground shadow-xl">
+            <Link className="text-xl font-semibold hover:font-bold hover:text-cyan-400" href="/">
                 Gruppe 2
             </Link>
 
             {/* Navigation Links */}
             <div className="flex space-x-6">
-                <Link className="hover:text-gray-400" href="/bill-of-materials">Bill of Materials</Link>
-                <Link className="hover:text-gray-400" href="/periods">Periods</Link>
+                <Link className="hover:text-cyan-400 hover:font-bold" href="/bill-of-materials">Bill of Materials</Link>
+                <Link className="hover:text-cyan-400 hover:font-bold" href="/periods">Periods</Link>
             </div>
         </div>
     );

e90a8965b409e4335f57a6bffaec839cd11beefe
Leon Gauweiler <leon.gauweiler@gmail.com>
2024-11-05 17:34:03 +0100

    fix: bom width

--- src/app/bill-of-materials/page.tsx
+++ src/app/bill-of-materials/page.tsx
@@ -39,7 +39,7 @@ export default async function BillOfMaterials() {
     const materials = await fetchMaterials();
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
             {materials.map((material: MaterialWithRequirements) => {
                 return (
                     <Card key={material.id} className="shadow-lg p-4">

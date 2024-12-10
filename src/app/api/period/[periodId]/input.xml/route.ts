098a8ea2f236e26e928884091346a86f506e9301
leon.gauweiler <70015546+Gauweiler42@users.noreply.github.com>
2024-12-10 13:24:14 +0100

    feat(api): Added directsell api support

--- src/app/api/period/[periodId]/input.xml/route.ts
+++ src/app/api/period/[periodId]/input.xml/route.ts
@@ -8,20 +8,68 @@ export async function GET(
   request: Request,
   { params }: { params: { periodId: string } }
 ) {
+  const extractNumbers = (materialId: string): string => {
+    const match = materialId.match(/\d+/); // Sucht nach einer oder mehreren Zahlen
+    return match ? match[0] : ""; // Gibt die erste gefundene Zahl zurück oder einen leeren String
+  };
+
   const periodId = parseInt(params.periodId);
 
   try {
     const doc = create({ version: "1.0" })
-      .ele("response")
-      .ele("periodId")
-      .txt(String(periodId))
-      .up()
-      .ele("status")
-      .txt("success")
-      .end({ prettyPrint: true });
+      .ele("input")
+      .ele("qualitycontrol")
+      .att("type", "no")
+      .att("losequantity", "0")
+      .att("delay", "0")
+      .up();
+
+    // Salewish
+    const salewishes = await prisma.forecast.findMany({
+      where: {
+        periodId: periodId,
+        forPeriod: periodId,
+      },
+      orderBy: {
+        materialId: "asc",
+      },
+    });
+
+    const sellwish = doc.ele("sellwish");
+    for (const salewish of salewishes) {
+      sellwish
+        .ele("item")
+        .att("article", extractNumbers(salewish.materialId))
+        .att("quantity", String(salewish.amount))
+        .up();
+    }
+    sellwish.up();
+
+    // Additional Sales
+    const additionalSales = await prisma.additionalSale.findMany({
+      where: {
+        periodId: periodId,
+        forPeriod: periodId,
+      },
+      orderBy: {
+        materialId: "asc",
+      },
+    });
+
+    const selldirect = doc.ele("selldirect");
+    for (const additionalSale of additionalSales) {
+      selldirect
+        .ele("item")
+        .att("article", extractNumbers(additionalSale.materialId))
+        .att("quantity", String(additionalSale.amount))
+        .att("price", "0.0")
+        .att("penalty", "0.0")
+        .up();
+    }
+    sellwish.up();
 
     // Rückgabe einer XML-Response
-    return new NextResponse(doc, {
+    return new NextResponse(doc.end({ prettyPrint: true }), {
       headers: {
         "Content-Type": "application/xml",
       },

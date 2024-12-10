8ebf0af0e93f2d31f0ca62f57292c477702ed28e
leon.gauweiler <70015546+Gauweiler42@users.noreply.github.com>
2024-12-10 23:13:52 +0100

    feat(home): Translation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,96 +1,96 @@
-// app/page.tsx
-
+import initTranslation from '@/i18n';
 import LineChartStatistic from "@/components/statistic/LineChartStatistic";
 
-export default function HomePage() {
-    return (
-        <div className="mt-10 w-full">
-            <h1 className="text-4xl font-bold text-center mb-8">Bicycle Calculation Tool</h1>
-            <div className="flex flex-col gap-4">
-              <LineChartStatistic
-                cardTitle="Combined Profits"
-                cardDescription="Here you can find the combined profits from all sale points"
-                types={
-                  [
-                    'summary_profit',
-                  ]
-                }
-                disableAverage={ false }
-              />
-              <LineChartStatistic
-                cardTitle="Profit streams"
-                cardDescription="Here you can find details of each profit stream"
-                types={
-                  [
-                    'normalsale_profit',
-                    'directsale_profit',
-                    'marketplacesale_profit'
-                  ]
-                }
-                disableAverage={ true }
-              />
-              <LineChartStatistic
-                cardTitle="Store Value"
-                cardDescription="Here you can see the value of our storage"
-                types={
-                  [
-                    'storevalue',
-                  ]
-                }
-                disableAverage={ false }
-              />
-              <LineChartStatistic
-                cardTitle="Storage Costs"
-                cardDescription="Here you can see our storage costs over the periods"
-                types={
-                  [
-                    'storagecosts',
-                  ]
-                }
-                disableAverage={ false }
-              />
-              <LineChartStatistic
-                cardTitle="Delivery Reliability"
-                cardDescription="Here you can see Delivery Reliability per period"
-                types={
-                  [
-                    'deliveryreliability',
-                  ]
-                }
-                disableAverage={ false }
-              />
-              <LineChartStatistic
-                cardTitle="Sales"
-                cardDescription="Here you can see the sales per period"
-                types={
-                  [
-                    'salesquantity',
-                  ]
-                }
-                disableAverage={ false }
-              />
-              <LineChartStatistic
-                cardTitle="Capacity"
-                cardDescription="Here you can see the possible capacity and the used capacity"
-                types={
-                  [
-                    'possiblecapacity',
-                    'productivetime'
-                  ]
-                }
-                disableAverage={ true }
-              />
-              <LineChartStatistic
-                cardTitle="Efficiency"
-                cardDescription="Here you can see the efficiency in %"
-                types={
-                  [
-                    'effiency',
-                  ]
-                }
-                disableAverage={ true }
-              />
-            </div>
-        </div>
-    );
+export default async function HomePage({ params }: { params: { locale: string } }) {
+  const { t } = await initTranslation(params.locale, ['home']);
+  return (
+      <div className="mt-10 w-full">
+          <h1 className="text-4xl font-bold text-center mb-8">Bicycle Calculation Tool</h1>
+          <div className="flex flex-col gap-4">
+            <LineChartStatistic
+              cardTitle={t("combined_profits")}
+              cardDescription={t("combined_profits_desc")}
+              types={
+                [
+                  'summary_profit',
+                ]
+              }
+              disableAverage={ false }
+            />
+            <LineChartStatistic
+              cardTitle={t("profit_streams")}
+              cardDescription={t("profit_streams_desc")}
+              types={
+                [
+                  'normalsale_profit',
+                  'directsale_profit',
+                  'marketplacesale_profit'
+                ]
+              }
+              disableAverage={ true }
+            />
+            <LineChartStatistic
+              cardTitle={t("store_value")}
+              cardDescription={t("store_value_desc")}
+              types={
+                [
+                  'storevalue',
+                ]
+              }
+              disableAverage={ false }
+            />
+            <LineChartStatistic
+              cardTitle={t("storage_costs")}
+              cardDescription={t("storage_costs_desc")}
+              types={
+                [
+                  'storagecosts',
+                ]
+              }
+              disableAverage={ false }
+            />
+            <LineChartStatistic
+              cardTitle={t("delivery_reliability")}
+              cardDescription={t("delivery_reliability_desc")}
+              types={
+                [
+                  'deliveryreliability',
+                ]
+              }
+              disableAverage={ false }
+            />
+            <LineChartStatistic
+              cardTitle={t("sales")}
+              cardDescription={t("sales_desc")}
+              types={
+                [
+                  'salesquantity',
+                ]
+              }
+              disableAverage={ false }
+            />
+            <LineChartStatistic
+              cardTitle={t("capacity")}
+              cardDescription={t("capacity_desc")}
+              types={
+                [
+                  'possiblecapacity',
+                  'productivetime'
+                ]
+              }
+              disableAverage={ true }
+            />
+            <LineChartStatistic
+              cardTitle={t("efficiency")}
+              cardDescription={t("efficiency_desc")}
+              types={
+                [
+                  'effiency',
+                ]
+              }
+              disableAverage={ true }
+            />
+          </div>
+      </div>
+  );
 }
